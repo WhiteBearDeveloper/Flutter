6563f35363e9c38ac196ec76ea6990ddf4bada09
alexeichuk@aunite.com <BZLdfzjYTeCvg5UoYLaj>
2020-09-24 14:14:23 +0700

    Add background and Sign in Title

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget{
+  @override
+
+  _loginScreenState createState() => _LoginScreenState();
+}
+
+class _loginScreenState extends State<LoginScreen>{
+  @override
+
+  Widget build(BuildContext context){
+    return Scaffold(
+
+    );
+  }
+}
