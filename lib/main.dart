a223d3afa20d5bb2316f40c56e42fab184d44d8f
alexeichuk@aunite.com <BZLdfzjYTeCvg5UoYLaj>
2020-10-05 14:37:32 +0700

    Add footnote links

--- lib/main.dart
+++ lib/main.dart
@@ -278,6 +278,31 @@ class _LoginScreenState extends State<LoginScreen>{
                         ],
                       ),
                     ),
+                      GestureDetector(
+                        onTap: () => print('Sign Up Button Pressed'),
+                        child: RichText(
+                          text: TextSpan(
+                            children: [
+                              TextSpan(
+                                text: 'Don\'t have an Account? ',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 18.0,
+                                  fontWeight: FontWeight.w400
+                                )
+                              ),
+                              TextSpan(
+                                  text: 'Sign Up',
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 18.0,
+                                      fontWeight: FontWeight.bold
+                                  )
+                              ),
+                            ],
+                          ),
+                        )
+                      )
                   ],
                 ),
               ),
