39fbdf8d91d007e53c31f1defc8bfd2e09722694
k.alekseychuk <k.alekseychuk@els24.com>
2021-01-29 17:37:20 +0700

    Add Container wrapper, edit height of Divider

--- lib/main.dart
+++ lib/main.dart
@@ -6,42 +6,47 @@ class MainScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         backgroundColor: Colors.black,
-        appBar: AppBar(title: Text('Экраны')),
-        body: new ListView(children: <Widget>[
-          ListTile(
-            leading: CircleAvatar(
-              child: Icon(
-                Icons.account_circle_outlined,
-                size: 40,
-                color: Colors.white,
+        body: Container(
+          height: double.infinity,
+          child: ListView(
+            children: <Widget>[
+              ListTile(
+                leading: CircleAvatar(
+                  child: Icon(
+                    Icons.account_circle_outlined,
+                    size: 40,
+                    color: Colors.white,
+                  ),
+                  backgroundColor: Colors.transparent,
+                ),
+                title: Text(
+                  'Login',
+                  style: TextStyle(
+                    color: Colors.white,
+                  )
+                ),
+                subtitle: Text(
+                  'Screen with login form',
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+                trailing: Icon(
+                  Icons.keyboard_arrow_right,
+                  color: Colors.white,
+                ),
+                onTap: () {
+                  Navigator.pushNamed(context, '/login-screen');
+                },
               ),
-              backgroundColor: Colors.transparent,
-            ),
-            title: Text(
-              'Login',
-              style: TextStyle(
+              Divider(
                 color: Colors.white,
-              )
-            ),
-            subtitle: Text(
-              'Screen with login form',
-              style: TextStyle(
-                color: Colors.white,
+                height: 1.0,
               ),
+            ],
             ),
-            trailing: Icon(
-              Icons.keyboard_arrow_right,
-              color: Colors.white,
-            ),
-            onTap: () {
-              Navigator.pushNamed(context, '/login-screen');
-            },
-          ),
-          Divider(
-            color: Colors.white,
-          ),
-        ] // ListView
-            ));
+        )
+    );
   }
 }
 
