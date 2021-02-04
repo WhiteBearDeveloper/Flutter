d80a0c9078048d17336ca36f194e69b3524fa5ee
k.alekseychuk <k.alekseychuk@els24.com>
2021-02-04 15:30:28 +0700

    Rollback generate list, some attempt create array with screens data

--- lib/main.dart
+++ lib/main.dart
@@ -1,72 +1,31 @@
 import 'package:flutter/material.dart';
 import './screens/loginScreen.dart';
 
-class ScreensList extends StatefulWidget{
+class ScreenListParams{
+  String name;
+  String description;
+  String link;
+}
+
+class ScreensList extends StatefulWidget {
   @override
   createState() => new ScreensState();
 }
 
-class ScreensState extends State<ScreensList>{
-  List<String> _array = [];
+class ScreensState extends State<ScreensList> {
+  List<ScreenListParams> _array = [
+    {
+      name: 'Login',
+      description: 'Screen with login form',
+      link: '/login-screen'
+    }
+  ];
 
   @override
-
   Widget build(BuildContext context) {
-    return new ListView.builder(itemBuilder: (context, i){
-      print('num $i : нечетное = ${i.isOdd}');
 
-      if(i.isOdd) return new Divider();
-
-      final int index = i ~/ 2;
-
-      print('index $index');
-      print('length ${_array.length}');
-
-      if (index >= _array.length) _array.addAll(['$index', '${index + 1}', '${index + 2}']);
-
-      return new ListTile(title: new Text(_array[index]));
-    });
   }
 }
-
-// ListView(
-// padding: EdgeInsets.only(top: 40.0),
-// children: <Widget>[
-// ListTile(
-// leading: CircleAvatar(
-// child: Icon(
-// Icons.account_circle_outlined,
-// size: 40,
-// color: Colors.white,
-// ),
-// backgroundColor: Colors.transparent,
-// ),
-// title: Text(
-// 'Login',
-// style: TextStyle(
-// color: Colors.white,
-// )
-// ),
-// subtitle: Text(
-// 'Screen with login form',
-// style: TextStyle(
-// color: Colors.white,
-// ),
-// ),
-// trailing: Icon(
-// Icons.keyboard_arrow_right,
-// color: Colors.white,
-// ),
-// onTap: () {
-// Navigator.pushNamed(context, '/login-screen');
-// },
-// ),
-// Divider(
-// color: Colors.white,
-// height: 1.0,
-// ),
-// ],
-// ),
 
 class MainScreen extends StatelessWidget {
   @override
@@ -81,22 +40,53 @@ class MainScreen extends StatelessWidget {
               SizedBox(
                 height: 80.0,
               ),
-              Text(
-                  "All Screens",
+              Text("All Screens",
                   style: TextStyle(
                       color: Colors.white,
                       fontFamily: 'OpenSans',
                       fontSize: 30.0,
-                      fontWeight: FontWeight.bold
-                  )
-              ),
+                      fontWeight: FontWeight.bold)),
               Expanded(
-                child: new ScreensList(),
+                child: ListView(
+                  padding: EdgeInsets.only(top: 40.0),
+                  children: <Widget>[
+                    ListTile(
+                      leading: CircleAvatar(
+                        child: Icon(
+                          Icons.account_circle_outlined,
+                          size: 40,
+                          color: Colors.white,
+                        ),
+                        backgroundColor: Colors.transparent,
+                      ),
+                      title: Text('Login',
+                          style: TextStyle(
+                            color: Colors.white,
+                          )),
+                      subtitle: Text(
+                        'Screen with login form',
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
+                      trailing: Icon(
+                        Icons.keyboard_arrow_right,
+                        color: Colors.white,
+                      ),
+                      onTap: () {
+                        Navigator.pushNamed(context, '/login-screen');
+                      },
+                    ),
+                    Divider(
+                      color: Colors.white,
+                      height: 1.0,
+                    ),
+                  ],
+                ),
               )
             ],
           ),
-        )
-    );
+        ));
   }
 }
 
