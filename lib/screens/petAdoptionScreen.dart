82a089e44135d4518bf084f7b8f08511677ca5ff
k.alekseychuk <k.alekseychuk@els24.com>
2021-03-02 22:47:17 +0700

    Create new screen, update routes and screens lists; begin work under new screen

--- lib/screens/petAdoptionScreen.dart
+++ lib/screens/petAdoptionScreen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class Owner {
@@ -36,7 +37,7 @@ class Pet {
 
 var owner = Owner(
     name: 'Roselia Drew',
-    imageUrl: 'assets/images/user.png',
+    imageUrl: 'lib/images/petAdoptionScreen/user.jpg',
     bio:
     'I recently lost my job and don\'t have enough time or money to tend to Darlene anymore. I have a lot of health issues that need attention and want to give Darlene the best life possible.');
 var pets = [
@@ -68,6 +69,35 @@ class petAdoptionScreenMain extends StatefulWidget{
 }
 
 class _petAdoptionScreenMainState extends State<petAdoptionScreenMain>{
+  Widget petCategory(bool isSelected, String category){
+    return GestureDetector(
+      onTap: () => print('Select category'),
+      child: Container(
+        margin: EdgeInsets.all(10.0),
+        width: 80.0,
+        decoration: BoxDecoration(
+            color: isSelected ? Theme.of(context).primaryColor : Color(0xFFF8F2F7),
+            borderRadius: BorderRadius.circular(20.0),
+            border: isSelected ? Border.all(
+              width: 8.0,
+              color: Color(0xFFFED8D3)
+            ) : null,
+        ),
+        child: Center(
+          child: Text(
+            category,
+            style: TextStyle(
+                fontFamily: 'Montserrat',
+                fontSize: 14.0,
+                fontWeight: FontWeight.w600,
+                color: Colors.black
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context){
     return Scaffold(
@@ -75,6 +105,11 @@ class _petAdoptionScreenMainState extends State<petAdoptionScreenMain>{
       body: ListView(
         children: <Widget>[
           Container(
+            padding: EdgeInsets.only(
+              left: 40.0,
+              top: 40.0
+            ),
+            alignment: Alignment.centerLeft,
             child: CircleAvatar(
               child: ClipOval(
                 child: Image(
@@ -84,6 +119,68 @@ class _petAdoptionScreenMainState extends State<petAdoptionScreenMain>{
                   fit: BoxFit.cover
                 ),
               ),
+            ),
+          ),
+          SizedBox(
+            height: 40.0,
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(
+                horizontal: 40.0
+            ),
+            child: TextField(
+              style: TextStyle(
+                fontFamily: 'Montserrat',
+                fontSize: 22.0
+              ),
+              decoration: InputDecoration(
+                border: InputBorder.none,
+                prefixIcon: Padding(
+                  padding: EdgeInsets.only(
+                      right: 30.0
+                  ),
+                  child: Icon(
+                    Icons.add_location,
+                    color: Colors.black,
+                    size: 40.0,
+                  ),
+                ),
+                labelText: 'Location',
+                labelStyle: TextStyle(
+                  fontSize: 20.0,
+                  color: Colors.grey
+                ),
+                contentPadding: EdgeInsets.only(
+                  bottom: 20.0
+                )
+              ),
+            ),
+          ),
+          Container(
+            height: 100.0,
+            child: ListView(
+              scrollDirection: Axis.horizontal,
+              children: <Widget>[
+                SizedBox(
+                  width: 40.0,
+                ),
+                Padding(
+                  padding: EdgeInsets.only(
+                    right: 20.0
+                  ),
+                  child: IconButton(
+                    onPressed: () => print('Filters'),
+                    icon: Icon(
+                      Icons.search,
+                      size: 35.0
+                    ),
+                  ),
+                ),
+                petCategory(false, 'Cats'),
+                petCategory(true, 'Dogs'),
+                petCategory(false, 'Birds'),
+                petCategory(false, 'Other'),
+              ],
             ),
           )
         ],
